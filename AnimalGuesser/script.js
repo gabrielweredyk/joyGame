eeb48fd97eb9e5ccb2dc92e1a9425c069607545f
gweredyk <47723493+gabrielweredyk@users.noreply.github.com>
2020-03-20 20:33:00 -0400

    Joyful Update
    did your stuff

--- AnimalGuesser/script.js
+++ AnimalGuesser/script.js
@@ -3,26 +3,19 @@ var first = true;
 var score = 0;
 var finished = false;
 
+var jump = true;
+
+var selected = 0;
+
 var rounds = 10;
 
-var animals = [
-    ["bunny", "rabbit"],
-    ["cat", "kitten", "kitty"],
-    ["chick", "duck", "bird"],
-    ["dog", "puppy"],
-    ["hedgehog", "porcupine"],
-    ["lizard", "newt"],
-    ["penguin"],
-    ["pony", "horse"],
-    ["seal"],
-    ["squirrel", "chipmunk"]
-];
+var animals = ["bunny", "cat", "chick", "dog", "hedgehog", "lizard", "penguin", "pony", "seal", "squirrel"];
 
 var stage = Math.floor(Math.random() * 10);
 
 function ScoreDecreaser() {
     if (!finished) {
-        if (time > 4) {
+        if (score > 4 || time > 4) {
             time -= 4;
             document.getElementById("score").innerHTML = (score + time).toString();
             document.getElementById("hidden-img").style.filter = "blur(" + (time / 4).toString() + "px)";
@@ -32,19 +25,41 @@ function ScoreDecreaser() {
 }
 
 function CorrectGuess() {
+    var inputs = document.getElementById("inputs");
+    var img = document.getElementById("hidden-img");
 
-    time = 100;
-    rounds--;
+
     stage = Math.floor(Math.random() * 10);
-    document.getElementById("hidden-img").setAttribute("src", "img/" + animals[stage][0] + ".png");
-    document.getElementById("hidden-img").style.filter = "blur(25px)";
-    document.getElementById("input").value = "";
-    document.getElementById("input").focus();
+
+    while (inputs.children.length != 0) {
+        inputs.removeChild(inputs.children[0]);
+    }
+
+    img.style.width = (window.innerWidth / 2).toString() + "px";
+    img.style.height = (window.innerHeight / 2).toString() + "px";
+
+    for (i = 0; i < animals[stage].length; i++) {
+        let input = document.createElement("INPUT");
+        input.setAttribute("type", "text");
+        input.setAttribute("autocomplete", "off");
+        input.setAttribute("oninput", "WordCheck(" + i.toString() + ")");
+        input.setAttribute("maxlength", "1");
+        input.setAttribute("id", i.toString());
+        inputs.appendChild(input);
+    }
+
+    inputs.children[0].focus();
+
+    rounds--;
+    img.setAttribute("src", "img/" + animals[stage] + ".png");
+    img.style.filter = "blur(25px)";
     if (0 > rounds) {
         finished = true;
         document.getElementById("score-type").innerHTML = "Final Score:";
-        document.getElementById("hidden-img").style.visibility = "hidden";
-        document.getElementById("input").style.visibility = "hidden";
+        img.style.visibility = "hidden";
+        while (inputs.children.length != 0) {
+            inputs.removeChild(inputs.children[0]);
+        }
     }
     else if (first) {
         ScoreDecreaser();
@@ -52,22 +67,62 @@ function CorrectGuess() {
     }
     else {
         score += time;
+        time = 100;
     }
 }
 
-function WordCheck() {
-    if (finished) {
-        document.getElementById("input").value = "";
-    }
-    else {
-        var input = document.getElementById("input").value;
-        console.log(input);
-        for (i = 0; i < animals[stage].length; i++) {
-            console.log(animals[stage][i]);
-            if (input == animals[stage][i]) {
-                CorrectGuess();
-                break;
-            }
+function WordCheck(x) {
+
+    var inputs = document.getElementById("inputs");
+    var guess = "";
+
+    if (x != inputs.children.length - 1) {
+        if (jump) {
+            inputs.children[x + 1].focus();
+        }
+        else {
+            jump = true;
         }
     }
+    for (i = 0; i < inputs.children.length; i++) {
+        guess += inputs.children[i].value;
+    }
+    if (guess == animals[stage]) {
+        CorrectGuess();
+    }
 }
+
+document.addEventListener("keydown", function (event) {
+    var inputs = document.getElementById("inputs");
+    var active = document.activeElement.id;
+
+    if (!isNaN(parseInt(active))) {
+        selected = parseInt(active);
+    }
+        
+    if (finished) { return; }
+    if (event.keyCode === 8) {
+        if (inputs.children[selected].value == "") {
+            let temp = inputs.children[selected - 1].value;
+            console.log(temp);
+            inputs.children[selected - 1].focus();
+            inputs.children[selected - 1].value = temp;
+            jump = false;
+        }
+        else {
+            inputs.children[selected].value = "";
+        }
+    }
+    else if (event.keyCode === 37) {
+        if (selected != 0) {
+            inputs.children[selected - 1].focus();
+        }
+        jump = false;
+    }
+    else if (event.keyCode === 39) {
+        if (selected != inputs.children.length - 1) {
+            inputs.children[selected + 1].focus();
+        }
+        jump = false;
+    }
+});
--- ScratchOff/sketch.js
+++ ScratchOff/sketch.js
@@ -1,20 +1,31 @@
 var images = 5;
 
-let img;
-let dog;
+var img;
+var dog;
 
 var points = [];
 var previous;
 var first = true;
+var size = 0;
+
+var tints = ["#B67856", "#BCBCBC", "#BCBCBC", "#BCBCBC", "#BCBCBC", "#E2CC99", "#D9CA8F", ];
+var selected = [1, 0, 0, 0, 0, 0, 0];
+var sizes = [37.5, 41.75, 35.25, 47.75, 60.25, 52.15, 47.5];
+//           2     3       1      4      6       5      4
+var pen_sizes = [2, 4, 2, 6, 10, 8, 6];
+var names = ["Penny", "Nickel", "Dime", "Quarter", "Half Dollar", "Dollar", "Half Euro"];
+
+var colors = [Math.floor(Math.random() * 128), Math.floor(Math.random() * 128), Math.floor(Math.random() * 128)];
 
 function setup() {
-    createCanvas(window.innerWidth - 10, window.innerHeight - 20);
+    createCanvas(800, 700);
     dog = loadImage("img/" + (Math.floor(Math.random() * 5) + 1).toString() + ".png");
-    img = createImage(window.innerWidth - 10, window.innerHeight - 20);
+    //dog = loadImage("https://cdn1.creativecirclemedia.com/liherald/original/20191114-115356-89714%20A%20MER%20WILD%20ART%20AS%2011_14_NORMAL_CMYK.jpg");
+    img = createImage(800, 600);
     img.loadPixels();
     for (let i = 0; i < img.width; i++) {
         for (let j = 0; j < img.height; j++) {
-            img.set(i, j, color(150, 150, 150, 255));
+            img.set(i, j, color(colors[0], colors[1], colors[2], 255));
         }
     }
     previous = [mouseX, mouseY];
@@ -22,16 +33,31 @@ function setup() {
 
 function draw() {
     cursor(HAND);
-    background(dog);
+    background(50);
+    image(dog, 0, 0, 800, 600);
     if (!first) {
         gap();
     }
     else {
         previous = [mouseX, mouseY];
+    }
+    if (mouseX != 0 && mouseY != 0) {
+        if (first) {
+            console.log(mouseX);
+            console.log(mouseY);
+        }
         first = false;
     }
+    coin();
     img.updatePixels();
     image(img, 0, 0);
+    if (first) {
+        textAlign(CENTER, CENTER);
+        textSize(32);
+        fill("white");
+        noStroke();
+        text("Move your mouse to reveal what's underneath!", 400, 300);
+    }
 }
 
 function gap() {
@@ -54,8 +80,8 @@ function gap() {
 
     }
     for (i = 0; i < points.length; i++) {
-        for (j = -5; j <= 5; j++) {
-            for (k = -5; k <= 5; k++) {
+        for (j = -pen_sizes[size]; j <= pen_sizes[size]; j++) {
+            for (k = -pen_sizes[size]; k <= pen_sizes[size]; k++) {
                 img.set(points[i][0] + j, points[i][1] + k, color(0, 0, 0, 0));
             }
         }
@@ -63,3 +89,33 @@ function gap() {
     previous = [mouseX, mouseY];
 }
 
+function coin() {
+    textAlign(CENTER, CENTER);
+    for (i = 0; i < tints.length; i++) {
+        fill("white");
+        textSize(14);
+        text(names[i], (i * 100) + 50, 645 - (sizes[i] / 2));
+        fill(tints[i])
+        circle((i * 100) + 50, 660, sizes[i] + selected[i]);
+    }
+    fill("white");
+    text("Select a coin!", 730, 650);
+}
+
+function mousePressed() {
+
+    if (mouseY < 600) { return; }
+    selected = [0, 0, 0, 0, 0, 0, 0];
+    let distances = [];
+    for (i = 0; i < 7; i++) {
+        distances.push([distance((i * 100) + 50, 660, mouseX, mouseY), i]);
+    }
+    distances.sort(function (a, b) { return a[0] - b[0] });
+    size = distances[0][1];
+    selected[distances[0][1]] = 5;
+    console.log(size);
+}
+
+function distance(x1, y1, x2, y2) {
+    return Math.sqrt(((y2 - y1) ** 2) + ((x2 - x1) ** 2));
+}
